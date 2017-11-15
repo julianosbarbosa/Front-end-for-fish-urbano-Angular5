6ff7ed46a6fd38f12de733c3cfe116e6ada82526
Juliano <juliano@nationphone.com>
2017-11-15 18:35:01 -0200

    UPDATE - Criação de Serviços e Promises, API Rest fake, injeção de dependências e HTTP Request, componente diversao e restaurantes

---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,19 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Oferta } from './shared/oferta.model';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class OfertasService {
+
+    constructor(private http: Http) { }
+
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta: any) => resposta.json());
+    }
+
+
+}
