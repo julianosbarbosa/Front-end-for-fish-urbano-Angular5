cf6b64185eb423b5b1979b74a27f4a4556ab6aa1
Juliano <juliano@nationphone.com>
2017-11-15 19:47:44 -0200

    UPDATE - Rotas e Navegação

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { Component } from '@angular/core/src/metadata/directives';
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'restaurantes', component: RestaurantesComponent },
+    { path: 'diversao', component: DiversaoComponent }
+];
+
